21bdb5e9246cec9f8f32522e6fd54740f7001e9d
Mark <mark@markjwood.com>
2021-09-20 15:06:24 -0700

    deleted an accidental extra close div from createCards function; gave unique 'name' id's to each card's h3

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,12 @@ function createCards(people) {
 		card.className = 'card';
 		card.innerHTML = `
       <div class="card-img-container">
-          <img class="card-img" src="${person.picture.thumbnail}">
-        </div>
-        <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-          <p class="card-text">${person.email}</p>
-				  <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
-        </div>
+        <img class="card-img" src="${person.picture.thumbnail}">
+      </div>
+      <div class="card-info-container">
+        <h3 id="card${person.name.first}${person.name.last}" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+        <p class="card-text">${person.email}</p>
+        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
       </div>
     `;
 		galleryDiv.appendChild(card);
